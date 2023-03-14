8899d15766954911595cdc15eb78626051e66680
Thomas Ortega II <tom.ortega@gmail.com>
2023-03-14 08:14:49 -0400

    create basic workflow for finding an exercise
    * fetch data based on selection
    * show data results
    * remove placeholder code
    
    Signed-off-by: Thomas Ortega II <tom.ortega@gmail.com>

--- QuickSweat/ContentView.swift
+++ QuickSweat/ContentView.swift
@@ -12,66 +12,53 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @EnvironmentObject private var appModel:AppModel
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        TabView {
+            NavigationView {
+                VStack {
+                    List {
+                        Text("choose_muscle")
+                        ForEach(appModel.muscleGroups, id:\.self) { muscle in
+                            NavigationLink {
+                                List {
+                                    ForEach(appModel.exercises, id:\.name) { exercise in
+                                        NavigationLink {
+                                            VStack (alignment:.leading) {
+                                                ScrollView {
+                                                    Text(exercise.name)
+                                                    Text(exercise.instructions)
+                                                }
+
+                                            }.frame(alignment:.topLeading)
+                                        } label: {
+                                            Text(exercise.name.capitalized)
+                                        }
+                                    }
+                                }.onAppear{
+                                    appModel.findExercises(muscle: muscle)
+                                }.navigationBarTitle("exercises")
+                            } label: {
+                                Text(LocalizedStringKey(muscle))
+                            }
+                        }
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                    .navigationBarTitle("muscle_group")
                 }
             }
-            Text("Select an item")
-        }.onAppear{
-            appModel.findExercises(muscle: "biceps")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            .tabItem{
+                VStack {
+                    Image(systemName: "figure.run")
+                        .foregroundColor(Color.black)
+                }
             }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            VStack {
+                Text("faves")
+            }
+            .tabItem{
+                VStack {
+                    Image(systemName: "hand.thumbsup.fill")
+                        .foregroundColor(Color.black)
+                }
             }
         }
     }
